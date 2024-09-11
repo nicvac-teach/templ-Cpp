82bde481f26349ef0baeeea9f95d4e2b8abfa53b
nicvac-teach <nicola.vacca@marconibari.edu.it>
2024-09-11 20:49:32 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Istruzioni
+- Aprire il file main.cpp
+- Scrivere il programma
+- Eseguire il programma:
+   - [Run and Debug] sulla sinistra
+   - selezionare C++
+   - Il programma è eseguito in TERMINAL
---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string nome;
+    cout << "Inserisci il tuo nome: ";
+    cin >> nome;
+    cout << "Ciao " <<nome<< "!" << endl;
+}
+
+//Per eseguire il programma: 
+// - sulla sinistra, [Run and Debug] e selezionare C++
+// - Il programma è eseguito in TERMINAL
